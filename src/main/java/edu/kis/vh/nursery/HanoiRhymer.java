02caefac39de731f8f2adacd8215bc0f2db512cf
HannaSauchuk <248664@edu.p.lodz.pl>
2024-05-20 18:12:11 +0200

    8.Przypisanie annotacji @Override

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,10 +4,11 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     int totalRejected = 0;
 
+
     public int reportRejected() {
         return totalRejected;
     }
-
+    @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
