eee73d21ef3eac3cde0ad5f16fb221ab7d67fd48
Michal Strozak <245931@edu.p.lodz.pl>
2024-06-02 12:12:21 +0200

    14. Zmiana klasy Node na prywatną, przeniesienie klasy Node do klasy IntLinkedList oraz usunięcie pliku Node.java

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    public static final int EMPTY = -1;
+    private static final int EMPTY = -1;
     private Node last;
     private int i;
 
@@ -37,4 +37,36 @@ public class IntLinkedList {
         last = last.getPrev();
         return ret;
     }
+
+    private class Node {
+
+        private final int value;
+        private Node prev;
+        private Node next;
+
+        public Node(int i) {
+            value = i;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
 }
