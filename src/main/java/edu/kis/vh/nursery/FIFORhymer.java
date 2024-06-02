c8cd8fcae22b592d689e16ff9d7af538296ea5eb
Michal Strozak <245931@edu.p.lodz.pl>
2024-06-02 13:20:44 +0200

    17. Dodanie dokumentacji do klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,20 @@
 package edu.kis.vh.nursery;
 
+/**
+ * The FIFORhymer class extends the DefaultCountingOutRhymer to provide
+ * a FIFO (First In, First Out) counting out mechanism.
+ */
+
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
+    /**
+     * Overrides the countOut method.
+     * Values are counted out in the order they were counted in.
+     *
+     * @return The first value that was counted in.
+     */
 
     @Override
     public int countOut() {
